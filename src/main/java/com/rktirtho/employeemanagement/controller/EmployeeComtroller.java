0b4b762977f7d9d890bfd13712d60844d61a6db1
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-01 17:55:07 +0600

    cross origin annotation added for specific port

--- src/main/java/com/rktirtho/employeemanagement/controller/EmployeeComtroller.java
+++ src/main/java/com/rktirtho/employeemanagement/controller/EmployeeComtroller.java
@@ -3,13 +3,14 @@ package com.rktirtho.employeemanagement.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rktirtho.employeemanagement.model.Employee;
 import com.rktirtho.employeemanagement.repository.EmployeeRepository;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/v1")
 public class EmployeeComtroller {
