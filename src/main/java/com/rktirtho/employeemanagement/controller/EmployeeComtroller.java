6460ebe33ea722f605aad1143dc708d9be3fd966
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-01 19:02:28 +0600

    employee add api added

--- src/main/java/com/rktirtho/employeemanagement/controller/EmployeeComtroller.java
+++ src/main/java/com/rktirtho/employeemanagement/controller/EmployeeComtroller.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,11 @@ public class EmployeeComtroller {
 	@GetMapping("employees")
 	public List<Employee> getAll(){
 		return repository.findAll();
+	}
+	
+	@PostMapping("addemployee")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return repository.save(employee);
 	}
 
 }
